f80ff4e1a2810b9bfbc51f43c9dfb265bf09c42d
Asif <Asif@Mo-As-If.home>
2019-02-13 14:34:28 -0500

    self portrait added

--- basic_example/selfportrait.js
+++ basic_example/selfportrait.js
@@ -5,7 +5,7 @@ function setup() {
     stroke(0);
     noFill();
     rectMode(CENTER);
-    var cnv = createCanvas(800, 400);
+    var cnv = createCanvas(700, 700);
     //var cnv = createCanvas(windowWidth, windowHeight);
     cnv.style('display', 'block');
     cnv.parent('content');
@@ -14,14 +14,50 @@ function setup() {
 }
 
 function draw() {
-    background(100,50,0);
+    background('#F0FFFF');
+    noStroke()
     translate(width/2, height/2);
-    ellipse(0,0,170,200);
+    fill('#ffcb9a');
+    ellipse(0,0,300,350);
+
+    //hat
+    fill('#333');
+    arc(0, 0, 300, 350, PI+.3, -.4, OPEN);
+
+    //eyes
     fill(255);
-    ellipse(15,0,15,20);
-    ellipse(-15,0,15,20);
-    ellipse(-15,4,7,10);
-    ellipse(15,4,7,10);
+    ellipse(30,0,40,50);
+    ellipse(-30,0,40,50);
+
+    //ears
+    fill('#ffcb9a');
+    ellipse(155,0,30,50);
+    ellipse(-155,0,30,50);
+
+    //earrings
+    fill(0)
+    ellipse(156, 16, 10, 10);
+    ellipse(-156, 16, 10, 10);
+
+    //pupil
+    fill(0);
+    ellipse(-20,10,10,13);
+    ellipse(20,10,10,13);
+
+    //eyebrows
+    stroke('#333');
+    strokeWeight(5);
+    noFill();
+    arc(32, 0, 80, 80, PI+.9, -.9);
+    arc(-32, 0, 80, 80, PI+.9, -.9);
+    line(130, -70, 230, -83);
+
+    stroke('#CD853F');
+    strokeWeight(3);
+    noFill();
+    line(0, 30, 10, 50);
+    line(10, 50, 0, 50);
+    line(-30, 100, 3, 100);
 
 
   for (var i = -4; i < width; i++){
